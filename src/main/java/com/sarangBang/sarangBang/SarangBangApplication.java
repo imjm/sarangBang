0c3f2ffdb0034b4078baa416e9c57b141f9314c6
imjm <77835002+imjm@users.noreply.github.com>
2023-10-04 18:25:11 +0900

    first commit

---
+++ src/main/java/com/sarangBang/sarangBang/SarangBangApplication.java
@@ -0,0 +1,13 @@
+package com.sarangBang.sarangBang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarangBangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarangBangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarangBang/sarangBang/SarangBangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarangBang.sarangBang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarangBangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
