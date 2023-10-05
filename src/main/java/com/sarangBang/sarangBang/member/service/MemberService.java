fd3e28c28c0d48aa6c0951bd6d9227ab4eea1c4d
imjm <77835002+imjm@users.noreply.github.com>
2023-10-05 18:33:04 +0900

    feat: 회원 저장 및 중복 검증 메서드

---
+++ src/main/java/com/sarangBang/sarangBang/member/service/MemberService.java
@@ -0,0 +1,31 @@
+package com.sarangBang.sarangBang.member.service;
+
+import com.sarangBang.sarangBang.member.domain.Member;
+import com.sarangBang.sarangBang.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+
+    }
+
+
+}
